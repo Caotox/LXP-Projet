974871e55d9d42d308aef80318df456631074cfa
Antoine <arocq92@gmail.com>
2025-10-21 17:24:02 +0200

    updt

--- Assets/Scripts/PiggyController.cs
+++ Assets/Scripts/PiggyController.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PiggyController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public InputActionReference horizontalMove;
+    public InputActionReference verticalMove;
+    public Vector3 moveDir;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,13 @@ public class PiggyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        //Move
+    }
+    void Move()
+    {
+        moveDir = new Vector3(horizontalMove.action.ReadValue<float>(), 0, verticalMove.action.ReadValue<float>());
+        transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
 
     }
 }
